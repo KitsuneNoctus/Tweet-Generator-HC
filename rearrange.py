87bd7cbb876a813ba90184cdac07caf0b13ea376
KitsuneNoctus <theceintiant@gmail.com>
2019-10-22 21:38:34 -0700

    Added reverse quote module.

--- rearrange.py
+++ rearrange.py
@@ -9,9 +9,19 @@ def rearrange_Quote(string):
         string_list.remove(random_word)
     return new_string
 
+def reverse_Quote(string):
+    new_string = ""
+    string_list = string.split()
+    for word in string.split():
+        new_string += (string_list[-1] + " ")
+        string_list.remove(string_list[-1])
+    return new_string
+    pass
+
 if __name__=='__main__':
     params = sys.argv[1:]
     #Example for use: "I am the best"
     string_of_words = str(params[0])
     print(rearrange_Quote(string_of_words))
+    #print(reverse_Quote(string_of_words))
     pass
