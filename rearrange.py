fb49072b54ecf288950a394f15096fbd2e790e65
KitsuneNoctus <theceintiant@gmail.com>
2019-10-22 21:18:48 -0700

    Adding rearrange module now

---
+++ rearrange.py
@@ -0,0 +1,13 @@
+import random
+
+def rearrange_Quote(string):
+    new_string = ""
+    string_list = string.split()
+    return string_list
+    # for word in string.split():
+    #     random_word = randint(0,)
+    # pass
+
+if __name__=='__main__':
+    print(rearrange_Quote("I am awesome"))
+    pass
