bdbe5829e17a856daaef018d98d3ecaaccf6ce9f
KitsuneNoctus <theceintiant@gmail.com>
2019-10-23 15:26:49 -0700

    Made code safe.

--- dictionary_words.py
+++ dictionary_words.py
@@ -5,11 +5,16 @@ import random, sys
 # f.close()
 # words_list = words_list.split()
 
-#Found this snippet of code from here: https://stackoverflow.com/questions/4666339/python-and-palindromes
-words_list = [line.strip() for line in open('/usr/share/dict/words')]
+# words_list = []
+def get_words():
+    #Found this snippet of code from here: https://stackoverflow.com/questions/4666339/python-and-palindromes
+    #Look up list comprhension
+    words_list = [line.strip() for line in open('/usr/share/dict/words')]
+    return words_list
 
 
 def create_random_sentence(sen_length):
+    words_list = get_words()
     sentence = ""
     while sen_length != 0:
         ran_word = random.choice(words_list)
