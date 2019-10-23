1e21f83967b642d0bd121f6315d7b91a0637d322
KitsuneNoctus <theceintiant@gmail.com>
2019-10-23 14:21:49 -0700

    Cleaned up code a little.

--- rearrange.py
+++ rearrange.py
@@ -5,7 +5,8 @@ def rearrange_Quote(string):
     new_string = ""
     string_list = string.split()
     for word in string.split():
-        random_word = string_list[random.randint(0,len(string_list)-1)]
+        # random_word = string_list[random.randint(0,len(string_list)-1)]
+        random_word = random.choice(string_list)
         if len(string_list)<2:
             new_string += (random_word)
         else:
@@ -21,12 +22,10 @@ def reverse_Quote(string):
         new_string += (string_list[-1] + " ")
         string_list.remove(string_list[-1])
     return new_string
-    pass
 
 if __name__=='__main__':
     params = sys.argv[1:]
     #Example for use: "I am the best"
     string_of_words = str(params[0])
     print(rearrange_Quote(string_of_words))
-    #print(reverse_Quote(string_of_words))
-    pass
+    # print(reverse_Quote(string_of_words))
