9fc8672c3b88badc86c6110ac39dca190b52f92f
KitsuneNoctus <theceintiant@gmail.com>
2019-10-26 14:32:40 -0700

    Creating word_frequency file.

---
+++ word_frequency.py
@@ -0,0 +1,16 @@
+# Counts the number of times each word in a piece of text appears
+
+def get_words():
+    #Found this snippet of code from here: https://stackoverflow.com/questions/4666339/python-and-palindromes
+    #Look up list comprhension
+    text_words = [line.strip() for line in open('pygmalion.txt')]
+    return text_words
+def histogram(text):
+
+    pass
+
+def unique_words(histogram):
+    pass
+
+def frequency(histogram,words):
+    pass
