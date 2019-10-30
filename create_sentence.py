ea81179e3372de2a2aa28e7a672ec7f2187f39ae
KitsuneNoctus <theceintiant@gmail.com>
2019-10-29 17:49:26 -0700

    Fixed small issue

--- create_sentence.py
+++ create_sentence.py
@@ -1,4 +1,6 @@
-import word_frequency, dictionary_words
+import word_frequency, dictionary_words, random, sys
 text = 'one fish two fish red fish blue fish'
-word_counts = word_frequency.histogram(text)
+
+text_stuff = text.split()
+word_counts = word_frequency.histogram(text_stuff)
 print(word_counts)
