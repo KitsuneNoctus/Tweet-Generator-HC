bba3dc228083f9cc6be9fb4410d8ca81912b7ab2
KitsuneNoctus <theceintiant@gmail.com>
2019-10-29 22:43:06 -0700

    Adding probability of words.

--- create_sentence.py
+++ create_sentence.py
@@ -11,6 +11,17 @@ def pick_one_word(histogram):
     one_entry = random.choice(list(histogram))
     return one_entry
 
+def calc_probability(histogram):
+    total_amount = 0
+    for i in histogram:
+        total_amount += histogram[i]
+
+    for entry in histogram:
+        rate = histogram[entry]/total_amount
+        print(f"{entry} -rate: {rate}")
+
+    pass
+
 if __name__=='__main__':
     # params = sys.argv[1:]
     #Example for use: "one fish two fish red fish blue fish"
@@ -18,3 +29,5 @@ if __name__=='__main__':
     # params[0]
     # print(convert_histogram(string_of_words))
     print(pick_one_word(convert_histogram(string_of_words)))
+    print("----------")
+    calc_probability(convert_histogram(string_of_words))
