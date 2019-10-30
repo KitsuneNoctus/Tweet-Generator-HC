4706625ab88f947231e68ed963a6742680e8fa3a
KitsuneNoctus <theceintiant@gmail.com>
2019-10-29 20:21:39 -0700

    Connvert to histogram created

--- create_sentence.py
+++ create_sentence.py
@@ -1,6 +1,12 @@
 import word_frequency, dictionary_words, random, sys
-text = 'one fish two fish red fish blue fish'
 
-text_stuff = text.split()
-word_counts = word_frequency.histogram(text_stuff)
-print(word_counts)
+def convert_histogram(text):
+    text_stuff = text.split()
+    word_counts = word_frequency.histogram(text_stuff)
+    return word_counts
+
+if __name__=='__main__':
+    params = sys.argv[1:]
+    #Example for use: "I am the best"
+    string_of_words = str(params[0])
+    print(convert_histogram(string_of_words))
