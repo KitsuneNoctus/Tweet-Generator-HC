35ce12d7289f58f9477f41c4ff7103bdcf0de3ab
KitsuneNoctus <theceintiant@gmail.com>
2019-11-03 15:38:43 -0800

    Making sure things can run

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def index():
     return word_probability.pick_one_word(word_probability.calc_probability(word_probability.convert_histogram()))
 
 if __name__ == '__main__':
-    app.run(debug=True) #, host='0.0.0.0', port=os.environ.get('PORT', 5000)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
