65fd9fd6710e442e6a8cacb2d9b19be4a75241c6
KitsuneNoctus <theceintiant@gmail.com>
2019-11-07 13:57:50 -0800

    Cleaned up code a little of the code based in code review.

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from bson.objectid import ObjectId
 from flask import Flask, render_template, request, redirect, url_for
 import os
-import word_probability
+from word_probability import pick_one_word, calc_probability, convert_histogram
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     """Return homepage."""
-    return word_probability.pick_one_word(word_probability.calc_probability(word_probability.convert_histogram()))
+    return pick_one_word(calc_probability(convert_histogram()))
+    # word_probability.pick_one_word(word_probability.calc_probability(word_probability.convert_histogram()))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
