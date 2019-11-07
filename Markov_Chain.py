4246c6bbbe5cc0c1e50cfbe1fa5b131b143464f5
KitsuneNoctus <theceintiant@gmail.com>
2019-11-07 15:53:11 -0800

    Adding markov chain and a test.

---
+++ Markov_Chain.py
@@ -0,0 +1,5 @@
+from word_probability import pick_one_word, calc_probability, convert_histogram
+from word_frequency import get_words
+from dictogram import Dictogram
+
+test_string = "I can no longer help what I feel in my heart. What is the point of living anymore? I am the king."
