f751fe0cfc6d75750d74b40b30a5f7da467ca4df
KitsuneNoctus <theceintiant@gmail.com>
2019-11-07 16:11:22 -0800

    Created a thing for testing Markov Chain.

--- Markov_Chain.py
+++ Markov_Chain.py
@@ -1,5 +1,13 @@
 from word_probability import pick_one_word, calc_probability, convert_histogram
 from word_frequency import get_words
 from dictogram import Dictogram
+from string import punctuation
 
 test_string = "I can no longer help what I feel in my heart. What is the point of living anymore? I am the king."
+
+text_words = []
+for word in test_string.split():
+    clean_word = word.strip(punctuation)
+    text_words.append(clean_word)
+
+print(text_words)
