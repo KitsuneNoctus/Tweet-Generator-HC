50544f89246db45a913fde00f0773dc4949025da
KitsuneNoctus <theceintiant@gmail.com>
2019-11-12 17:21:55 -0800

    Refactored Markov Chain based on some advice.

--- Markov_Chain.py
+++ Markov_Chain.py
@@ -22,30 +22,28 @@ class Markov_Chain(dict):
 
         """ Creating the Markov Chain """
         for index in range(len(self.word_list)-1):
-            # print(self)
             word = self.word_list[index]
             next_word = self.word_list[index+1]
 
             if word not in self:
-                self[word] = {}
-                self[word][next_word] = 1
+                small_dicto = Dictogram([next_word])
+                self[word] = small_dicto
+
             else:
-                dict_of_counts = self[word]
-                if next_word not in dict_of_counts:
-                    self[word][next_word] = 1
-                else:
-                    dict_of_counts[next_word] += 1
-
-
+                self[word].add_count(next_word)
 
     def creating_sentence(self, length = 10):
         """Create sentence using both dictogram and the markov chain just made."""
-        adding_word = self.dictionary_histogram.sample()
-        last_word = adding_word
         created_sentence = ""
+        adding_word = self.dictionary_histogram.sample()
         created_sentence += adding_word+" "
+
+        last_word = adding_word
+
         while length-1 > 0:
             if adding_word in self:
+                total = sum(self[adding_word].values())
+
                 print(self[adding_word])
                 print(sum(self[adding_word].values()))
                 pass
@@ -55,6 +53,7 @@ class Markov_Chain(dict):
                 # adding_word = word
             else:
                 pass
+
             length -= 1
 
 
@@ -63,6 +62,7 @@ class Markov_Chain(dict):
 if __name__=="__main__":
     # test_string = "I am. I was. I can only be. I will be king. It is very tiring. I am best."
     test_string = "I like dogs and you like dogs. I like cats but you hate cats."
+    print(test_string)
     first_chain = Markov_Chain(create_list(test_string))
     print(first_chain)
-    print(first_chain.creating_sentence())
+    # print(first_chain.creating_sentence())
